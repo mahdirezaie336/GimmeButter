f20fc0594d27f59622778d47beec2e9bf54db994
mahdi <mahdi.rezaie.336@gmail.com>
2021-04-19 02:18:04 +0430

    Finish successor without testing and handling blocks

--- main.py
+++ main.py
@@ -36,6 +36,83 @@ def parse_map() -> (list, State):
     return points, State(robot, butters)
 
 
+def successor(state: State) -> list:
+    next_states = []
+    robot_y, robot_x = state.robot[0], state.robot[1]
+
+    # Moving up handling
+    if robot_y > 0:
+        # Checking if there is a butter upside
+        if (robot_y-1, robot_x) not in state.butters:           # There is no butters upside
+            next_states.append(
+                State((robot_y-1, robot_x), state.butters.copy())
+            )
+        else:                                                   # There is a butter upside
+            # Butter not on bound condition
+            if robot_y != 1:
+                new_butters = state.butters.copy()
+                new_butters.remove((robot_y-1, robot_x))        # Moving butter
+                new_butters.append((robot_y-2, robot_x))
+                next_states.append(
+                    State((robot_y-1, robot_x), new_butters)
+                )
+
+    # Moving down handling
+    if robot_y < h - 1:
+        # Checking if there is a butter downside
+        if (robot_y + 1, robot_x) not in state.butters:         # There is no butters downside
+            next_states.append(
+                State((robot_y + 1, robot_x), state.butters.copy())
+            )
+        else:                                                   # There is a butter downside
+            # Butter not on bound condition
+            if robot_y != h - 2:
+                new_butters = state.butters.copy()
+                new_butters.remove((robot_y + 1, robot_x))      # Moving butter
+                new_butters.append((robot_y + 2, robot_x))
+                next_states.append(
+                    State((robot_y + 1, robot_x), new_butters)
+                )
+
+    # Moving right handling
+    if robot_x < w - 1:
+        # Checking if there is a butter right side
+        if (robot_y, robot_x + 1) not in state.butters:         # There is no butters right side
+            next_states.append(
+                State((robot_y, robot_x + 1), state.butters.copy())
+            )
+        else:                                                   # There is a butter right side
+            # Butter not on bound condition
+            if robot_x != w - 2:
+                new_butters = state.butters.copy()
+                new_butters.remove((robot_y, robot_x + 1))      # Moving butter
+                new_butters.append((robot_y, robot_x + 2))
+                next_states.append(
+                    State((robot_y, robot_x + 1), new_butters)
+                )
+
+    # Moving left handling
+    if robot_x > 0:
+        # Checking if there is a butter left side
+        if (robot_y, robot_x - 1) not in state.butters:         # There is no butters left side
+            next_states.append(
+                State((robot_y, robot_x - 1), state.butters.copy())
+            )
+        else:                                                   # There is a butter left side
+            # Butter not on bound condition
+            if robot_x != 1:
+                new_butters = state.butters.copy()
+                new_butters.remove((robot_y, robot_x - 1))      # Moving butter
+                new_butters.append((robot_y, robot_x - 2))
+                next_states.append(
+                    State((robot_y, robot_x - 1), new_butters)
+                )
+
+
+def ids_search(init_state: State):
+    pass
+
+
 def __main__():
 
     pass
--- node.py
+++ node.py
@@ -1,6 +1,9 @@
+from state import State
+
+
 class Node:
 
-    def __init__(self, state, parent, children=[]):
+    def __init__(self, state: State, parent, children=[]):
         self.state = state
         self.parent = parent
         self.children = children
