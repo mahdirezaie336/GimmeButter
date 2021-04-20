494b8e22c3c97aeb31bb8aa060ab1114ac62d656
mahdi <mahdi.rezaie.336@gmail.com>
2021-04-20 15:59:41 +0430

    Fix blocks colors

--- screen_manager.py
+++ screen_manager.py
@@ -17,6 +17,7 @@ class Display:
         sw, sh = Consts.SCREEN_WIDTH, Consts.SCREEN_HEIGHT
         self.screen = pygame.display.set_mode((sw, sh))
         self.screen.fill(Consts.BACKGROUND)
+
         # Setting cell size and other sizes
         if w / h > sw / sh:
             rect_width = sw - 2 * Consts.SCREEN_MARGIN_SIZE
@@ -27,14 +28,20 @@ class Display:
             cell_size = int(rect_height / h)
             rect_width = cell_size * w
 
+        # Drawing cells
         init_y = (sh - rect_height) / 2
         init_x = (sw - rect_width) / 2
         for j in range(h):
             for i in range(w):
-                pygame.draw.rect(self.screen, Consts.CELL_COLOR, (init_x + i * cell_size, init_y + j * cell_size,
-                                                                  cell_size, cell_size), 0)
-                pygame.draw.rect(self.screen, (0, 0, 0), (init_x + i * cell_size, init_y + j * cell_size,
-                                                          cell_size, cell_size), 1)
+                x = init_x + i * cell_size
+                y = init_y + j * cell_size
+                if map_array[j][i] == 'x':
+                    color = Consts.BLOCK_COLOR
+                else:
+                    color = Display.darker(Consts.CELL_COLOR, int(map_array[j][i]))
+                # Drawing Rectangles
+                pygame.draw.rect(self.screen, color, (x, y, cell_size, cell_size), 0)
+                pygame.draw.rect(self.screen, (0, 0, 0), (x, y, cell_size, cell_size), 1)
 
         pygame.display.update()
         # Threading part
@@ -44,7 +51,9 @@ class Display:
         pass
 
     def begin_display(self):
+
         def infinite_loop():
+            """ This is the function which includes the infinite loop for pygame pumping. """
             while True:
                 events = pygame.event.get()
                 for event in events:
@@ -54,5 +63,16 @@ class Display:
                 pygame.display.update()
                 pygame.time.wait(100)
 
+        # Starting thread
         self.display_thread = threading.Thread(name='Display', target=infinite_loop)
         self.display_thread.start()
+
+    @staticmethod
+    def darker(color: tuple[int, int, int], radius: int):
+        r = color[0] - (radius - 1) * 30
+        g = color[1] - (radius - 1) * 30
+        b = color[2] - (radius - 1) * 30
+        r = 0 if r < 0 else r
+        g = 0 if g < 0 else g
+        b = 0 if b < 0 else b
+        return r, g, b
