7c6c5d3f780296bd9aef7e5f59dea719d290cbc8
mahdi <mahdi.rezaie.336@gmail.com>
2021-05-02 20:18:47 +0430

    Change map from static class to normal

--- map.py
+++ map.py
@@ -1,14 +1,14 @@
 class Map:
 
-    def __init__(self, map_array: list[list[str]]):
+    def __init__(self, map_array: list[list[str]], h: int, w: int):
         self.map = map_array
+        self.width = w
+        self.height = h
 
     def get_item(self, y, x) -> str:
         return self.map[y][x]
 
     def append_row(self, row: list[str]) -> None:
-        n = len(row)
-        for i in self.map:
-            if len(i) != n:
-                raise Exception('Size Error: Number of columns is not same as previous rows.')
+        if len(row) != self.width:
+            raise ValueError('Invalid size of columns in this row:\n', str(row))
         Map.map.append(row)
